4bed31fb5917bda13ccfcc7f7a246e968b964f97
zerodeleo <julia.stjerna@hotmail.se>
2023-08-01 14:21:05 +0200

    add physics

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -1,25 +1,32 @@
-import * as THREE from "three";
 import { SCENE } from "../scene";
 import { CAMERA } from "../camera";
 import { RENDERER } from "../renderer";
 import { CUBE_MESH } from "../geometries";
 import { CONTROLS } from "../controls";
+import { WORLD } from "../physics/worlds";
+import { CUBE_BODY } from "../physics/bodies/intex";
+import * as THREE from "three";
 
-const DURATION = 1;
-const DELAY = 0;
-const SPEED = 0.3;
+const SPEED = 1;
+let oldElapsedTime = 0;
 
 const clock = new THREE.Clock();
 
 export const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-  const start = DURATION + DELAY;
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
+
+  // Update physics
+  WORLD.step(1 / 60, deltaTime, 3);
 
   // Update Object
-  if (elapsedTime > start) {
-    CUBE_MESH.rotation.x = (elapsedTime - start) * SPEED;
-    CUBE_MESH.rotation.y = (elapsedTime - start) * SPEED;
-  }
+  CUBE_MESH.position.x = CUBE_BODY.position.x;
+  CUBE_MESH.position.y = CUBE_BODY.position.y;
+  CUBE_MESH.position.z = CUBE_BODY.position.z;
+
+  // CUBE_MESH.rotation.x = elapsedTime * SPEED;
+  // CUBE_MESH.rotation.y = elapsedTime * SPEED;
 
   // Update controls
   CONTROLS.update();
--- src/geometries/index.ts
+++ src/geometries/index.ts
@@ -1,11 +1,31 @@
 import * as THREE from "three";
+
 import { MATERIAL_GRAY_400, MATERIAL_PINK_900 } from "../materials";
 
-const CUBE_GEOMETRY = new THREE.BoxGeometry(3, 3, 3);
+export const CUBE_VEC3_SIZE = new THREE.Vector3(3, 3, 3);
+const CUBE_GEOMETRY = new THREE.BoxGeometry(
+  CUBE_VEC3_SIZE.x,
+  CUBE_VEC3_SIZE.y,
+  CUBE_VEC3_SIZE.z
+);
 export const CUBE_MESH = new THREE.Mesh(CUBE_GEOMETRY, MATERIAL_PINK_900);
-CUBE_MESH.position.set(0, 0, 0);
+export const CUBE_VEC3_POSITION = new THREE.Vector3(0, 10, 0);
+CUBE_MESH.position.set(
+  CUBE_VEC3_POSITION.x,
+  CUBE_VEC3_POSITION.y,
+  CUBE_VEC3_POSITION.z
+);
 
-const PLANE_GEOMETRY = new THREE.PlaneGeometry(20, 20);
+export const PLANE_VEC2_SIZE = new THREE.Vector2(10, 10);
+const PLANE_GEOMETRY = new THREE.PlaneGeometry(
+  PLANE_VEC2_SIZE.x,
+  PLANE_VEC2_SIZE.y
+);
+export const PLANE_VEC3_POSITION = new THREE.Vector3(0, -1, 0);
 export const PLANE_MESH = new THREE.Mesh(PLANE_GEOMETRY, MATERIAL_GRAY_400);
-PLANE_MESH.position.set(0, -3, 0);
+PLANE_MESH.position.set(
+  PLANE_VEC3_POSITION.x,
+  PLANE_VEC3_POSITION.y,
+  PLANE_VEC3_POSITION.z
+);
 PLANE_MESH.rotation.x = -Math.PI * 0.5;
