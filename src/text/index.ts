f5d263c8d5718c85959f1ff9d41a2b8c27bbf385
zerodeleo <julia.stjerna@hotmail.se>
2023-08-01 14:21:47 +0200

    edit text position

--- src/text/index.ts
+++ src/text/index.ts
@@ -1,8 +1,8 @@
-import * as THREE from "three";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
-import { SCENE } from "../scene";
-import { MATERIAL_PINK_600 } from "../materials";
+import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { SCENE } from '../scene';
+import { MATERIAL_PINK_600 } from '../materials';
 
 const FONT_STYLE = {
   size: 0.4,
@@ -17,7 +17,7 @@ const FONT_STYLE = {
 
 const FONT_LOADER = new FontLoader();
 export const loadFont = () => {
-  FONT_LOADER.load("/fonts/optimer_regular.typeface.json", (font) => {
+  FONT_LOADER.load('/fonts/optimer_regular.typeface.json', (font) => {
     const TEXT_GEOMETRY = new TextGeometry(
       `Life is easy,
       
@@ -29,7 +29,7 @@ zerodeleo`,
       }
     );
     const TEXT_MESH = new THREE.Mesh(TEXT_GEOMETRY, MATERIAL_PINK_600);
-    TEXT_MESH.position.set(0, 5, 0);
+    TEXT_MESH.position.set(-5, 7, 0);
     TEXT_GEOMETRY.center();
     SCENE.add(TEXT_MESH);
   });
