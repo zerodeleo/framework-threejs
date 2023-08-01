8acbc58c0b4cbf379a146cf9badd02d128d7c06d
zerodeleo <julia.stjerna@hotmail.se>
2023-08-01 10:29:46 +0200

    add color config after new three update

--- src/camera/index.ts
+++ src/camera/index.ts
@@ -1,6 +1,6 @@
-import * as THREE from "three";
-import { SCENE } from "../scene";
-import { ASPECT_RATIO } from "../util";
+import * as THREE from 'three';
+import { SCENE } from '../scene';
+import { ASPECT_RATIO } from '../util';
 
 export const CAMERA = new THREE.PerspectiveCamera(90, ASPECT_RATIO);
 CAMERA.position.set(0, 0, 10);
