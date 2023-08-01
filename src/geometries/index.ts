fe73cc8cf0c4d113eecca13a0934f95a7e726a09
zerodeleo <julia.stjerna@hotmail.se>
2023-08-01 11:12:39 +0200

    add plane

--- src/geometries/index.ts
+++ src/geometries/index.ts
@@ -1,6 +1,11 @@
 import * as THREE from "three";
-import { MATERIAL_PINK_900 } from "../materials";
+import { MATERIAL_GRAY_400, MATERIAL_PINK_900 } from "../materials";
 
-const CUBE = new THREE.BoxGeometry(3, 3, 3);
-export const CUBE_MESH = new THREE.Mesh(CUBE, MATERIAL_PINK_900);
+const CUBE_GEOMETRY = new THREE.BoxGeometry(3, 3, 3);
+export const CUBE_MESH = new THREE.Mesh(CUBE_GEOMETRY, MATERIAL_PINK_900);
 CUBE_MESH.position.set(0, 0, 0);
+
+const PLANE_GEOMETRY = new THREE.PlaneGeometry(20, 20);
+export const PLANE_MESH = new THREE.Mesh(PLANE_GEOMETRY, MATERIAL_GRAY_400);
+PLANE_MESH.position.set(0, -3, 0);
+PLANE_MESH.rotation.x = -Math.PI * 0.5;
